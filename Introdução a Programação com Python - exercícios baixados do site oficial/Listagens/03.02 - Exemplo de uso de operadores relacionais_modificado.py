09bef8123543b8d70549c6fe93be5297dc1b522c
emersonleite <emerson.bl@gmail.com>
2018-04-03 13:44:24 +0000

    Envio de arquivos do livro Programação com Python

---
+++ Introdução a Programação com Python - exercícios baixados do site oficial/Listagens/03.02 - Exemplo de uso de operadores relacionais_modificado.py
@@ -0,0 +1,31 @@
+##############################################################################
+# Parte do livro Introdução à Programação com Python
+# Autor: Nilo Ney Coutinho Menezes
+# Editora Novatec (c) 2010-2014
+# Primeira edição - Novembro/2010 - ISBN 978-85-7522-250-8
+# Primeira reimpressão - Outubro/2011
+# Segunda reimpressão - Novembro/1012
+# Terceira reimpressão - Agosto/2013
+# Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
+# Site: http://python.nilo.pro.br/
+# 
+# Arquivo: capitulo 03\03.02 - Exemplo de uso de operadores relacionais.py
+##############################################################################
+
+
+
+
+
+
+a = 1       # a recebe 1
+b = 5       # b recebe 5
+c = 2       # c recebe 2
+d = 1       # d recebe 1
+print (a == b)     # a é igual a b ?
+print (b > a)       # b é maior que a?
+print (a < b)       # a é menor que b?
+print (a == d)     # a é igual a d?
+print (b >= a)     # b é maior ou igual a a?
+print (c <= b)     # c é menor ou igual a b?
+print (d != a)     # d é diferente de a?
+print (d != b)     # d é diferente de b?
